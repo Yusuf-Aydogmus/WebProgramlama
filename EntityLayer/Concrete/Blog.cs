5d959844528fdfea93227d7f47a80a06e1dbb2cb
Yusuf-Aydogmus <yuzarsif349@yandex.com>
2021-12-10 11:34:54 +0300

    İlişkiler eklendi ve databse üzerinden diyagramlar görüntülenmeye başlandı

--- EntityLayer/Concrete/Blog.cs
+++ EntityLayer/Concrete/Blog.cs
@@ -21,6 +21,7 @@ namespace EntityLayer.Concrete
 
         public int CategoryID { get; set; }
         public Category Category { get; set; }
+        public List<Comment> Comments { get; set; }
 
 
 
