1daec85f89fd87deed2fef3cc8b01c12606a7c72
Yusuf-Aydogmus <yuzarsif349@yandex.com>
2021-12-14 01:52:43 +0300

    Tema değişikliği yapıldı

---
+++ BlogProjem/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProjem.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index() //userlayoutu layout sayfası olarak tanımladık burada
+        {
+            return View();
+        }
+    }
+}
