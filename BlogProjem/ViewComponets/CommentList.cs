517e84b8e2be149f9b423bc1e57293d2e4fb2534
Yusuf-Aydogmus <yuzarsif349@yandex.com>
2021-12-15 13:45:14 +0300

    Veritabanından ilgili mevcut yorumları çekme işlemi tamamlandı ve yorum ile ilgili çalışmalar yapıldı.Hata viewcomponent kullanımı ile çözüldü

---
+++ BlogProjem/ViewComponets/CommentList.cs
@@ -0,0 +1,37 @@
+﻿using BlogProjem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProjem.ViewComponets
+{
+    public class CommentList : ViewComponent
+    {
+        //çağır-->İNVOKE
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    Username="Yusuf"
+                },
+                new UserComment
+                {
+                    ID=2,
+                    Username="Akif"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    Username="Kaan"
+                },
+            };
+            return View(commentvalues);
+        }
+
+    }
+}
---
+++ BlogProjem/ViewComponets/Comments/CommentListByBlog.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntitiyFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProjem.ViewComponets.Comments
+{
+    public class CommentListByBlog: ViewComponent
+    {
+        CommentManager cm = new CommentManager(new EFCommentRepository());
+        public IViewComponentResult Invoke() 
+        {
+            var values = cm.GetList(2);
+            return View(values);
+        }
+    }
+}
